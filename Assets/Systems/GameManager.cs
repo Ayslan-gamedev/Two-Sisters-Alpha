898f8de1cab187bac165563cc0e13394a5c03249
Ayslan-gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-07-07 12:38:46 -0300

    gameManager auterations

--- Assets/Systems/GameManager.cs
+++ Assets/Systems/GameManager.cs
@@ -1,62 +1,57 @@
+// System Library
 using System;
-using System.Collections;
-using System.Collections.Generic;
-
 using System.IO;
-
-using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Serialization;
-
+// Graphics Engine Library
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEditor;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-    //public PlayerInputActions playerInputs;
+    private protected byte pause = 0;
 
-    // 0 = Yellow Sister / 1 = Purple Sister
-    [SerializeField] protected GameObject[] sisters = new GameObject[2]; // GameObject referent of the players
-    [SerializeField] protected float[] startSpeed = new float[2], maxSpeed = new float[2], acceleration = new float[2];
-    [SerializeField] protected Vector2 atualMovDirection; // direction of movment seted (prolonged)
+    // Player Controll Variables
+    private const string PLAYER_OBJECT_KEY = "Player";
 
-    protected private bool pause = true;
-    protected private int playerSelected = 0;
-    protected private bool inMovment = false;
-    protected private float speed = 0;
+    [SerializeField] private protected Vector2[] playerLastSafePosition = new Vector2[2];
+    private protected GameObject[] sisters = new GameObject[2];
+    private protected byte currentPlayerSelected = 0;
+
+    private protected float currentSpeed;
+    private protected readonly float[] acceleration, slowdown, startSpeed, maxSpeed;
+    private protected Vector2 currentMoveDirection = Vector2.zero;
+    private protected bool inMovement = false;
+
+    // Save System Variables
+    private protected const string FILE_LOCATE_SAVE_KEY = "/SaveData/save";
+    private protected readonly string[] current_save_file = new string[3];
 
     // Awake is called before the scene starts
-    protected private void Awake() {
-        DontDestroyOnLoad(this.gameObject);
-        //playerInputs = GetComponent<PlayerInputActions>();
-
-        //playerInputs.PlayerActions.TouchTest.started += ctx => InputActions_onActionTriggered(ctx);
-        //playerInputs.PlayerActions.Movment.started += ctx => InputActions_onActionTriggered(ctx);
+    private protected void Awake() {
+        for(int i = 0; i < 3; i++) {
+            current_save_file[i] = Application.dataPath + FILE_LOCATE_SAVE_KEY + (i + 1) + ".dat";
+            if(!File.Exists(current_save_file[i]))
+                File.Create(current_save_file[i]);
+        }
     }
 
-    // Save game data and set start variables
-    protected private void SaveData(int saveID) {
+    // Update is called once per frame
+    private void Update() {
+        if(pause != 1) {
+            MovementSister();
 
+            if(inMovement == false && Input.GetKeyDown(KeyCode.P))
+                ChangeSister();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+            LoadScene(String.Empty,1);
     }
 
-    // Load game data and get start variables
-    protected private void LoadData(int saveID) {
-
-    }
-
-    private void InputActions_onActionTriggered(InputAction.CallbackContext context) {
-        Debug.Log(context.phase);
-        //Debug.Log(playerInputs.PlayerActions.TouchTest.ReadValue<Vector2>());
-    }
-
-    // Start is called in the first frame of the scene
-    protected private void Start() {
-        StartPhase(); // temporario
-    }
-
-    // Sets the direction the player chooses
-    protected private Vector2 MovDirection() {
+    // Sets the chosen direction based on player input
+    private protected Vector2 MoveDirecion() {
         if(Input.GetAxisRaw("Horizontal") > 0) return Vector2.right;
         else if(Input.GetAxisRaw("Horizontal") < 0) return Vector2.left;
         else if(Input.GetAxisRaw("Vertical") > 0) return Vector2.up;
@@ -64,85 +59,121 @@ public class GameManager : MonoBehaviour {
         else return Vector2.zero;
     }
 
-    // Update is called once per frame
-    private void Update() {
-        if(pause != false) {
-            MovementSister();
-            MovementCamera();
+    protected private void MovementSister() {
 
-            if(inMovment == false && Input.GetKeyDown(KeyCode.P)) ChangeSister();
+        // the player can only change the direction when the extended direction vector is equal to zero
+        if(currentMoveDirection == Vector2.zero) {
+            sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            inMovement = false;
+            currentSpeed = 0;
+
+            if(MoveDirecion().x != 0)
+                currentMoveDirection.x = MoveDirecion().x;
+            else if(MoveDirecion().y != 0)
+                currentMoveDirection.y = MoveDirecion().y;
         }
+        else {
+            inMovement = true;
 
-        if(Input.GetKeyDown(KeyCode.R)) LoadScene("none",1);
+            // Acceleration of speed
+            if(currentSpeed > maxSpeed[currentPlayerSelected])
+                currentSpeed = maxSpeed[currentPlayerSelected];
+            else {
+                if(currentSpeed < startSpeed[currentPlayerSelected])
+                    currentSpeed = startSpeed[currentPlayerSelected];
+                currentSpeed += acceleration[currentPlayerSelected] * Time.deltaTime;
+            }
+
+            sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = currentMoveDirection * currentSpeed;
+        }
+    }
+
+    private protected void ChangeSister() {
+        switch(currentPlayerSelected) {
+            case 1: currentPlayerSelected = 0; break;
+            case 0: currentPlayerSelected = 1; break;
+        }
+        sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 
     protected private void LoadScene(string scene,int sceneID) {
-        if(scene == "none") SceneManager.LoadSceneAsync(sceneID);
-        else SceneManager.LoadScene(scene);
-
-        StartPhase();
+        if(scene == String.Empty)
+            SceneManager.LoadSceneAsync(sceneID);
+        else
+            SceneManager.LoadScene(scene);
     }
 
-    // Load Start Atributts in the phase
-    protected private void StartPhase() {
-        // Find the sisters game objects in the scene
-        for(int i = 0; i < sisters.Length; i++) 
-            sisters[i] = GameObject.Find("Player" + i.ToString());
+    // Save Data
+    private protected void SaveData(int saveID) {
+        XmlSerializer ser = new XmlSerializer(typeof(GameData));
+        StreamWriter writer = new StreamWriter(current_save_file[saveID - 1]);
+
+        GameData dat = new GameData();
+
+        // insert here the data
+
+        dat.SaveID = saveID;
+
+        // ====================
+
+        ser.Serialize(writer,dat);
+        writer.Close();
     }
 
-    protected private void MovementSister() {
+    // Load Data
+    private protected void LoadData(int saveID) {
+        if(File.Exists(current_save_file[saveID - 1])) {
+            XmlSerializer xml = new XmlSerializer(typeof(GameData));
+            Stream reader = new FileStream(current_save_file[saveID - 1],FileMode.Open);
 
-        // o jogador so podera trocar a direção quando o vector de direção prolongada for igual a zero
-        if(atualMovDirection == Vector2.zero) {
-            sisters[playerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            inMovment = false;
-            speed = 0;
+            GameData dat = new GameData();
+            dat = (GameData)xml.Deserialize(reader);
 
-            if(MovDirection().x != 0)
-                atualMovDirection.x = MovDirection().x;
-            else if(MovDirection().y != 0)
-                atualMovDirection.y = MovDirection().y;
-        }
-        else {
-            inMovment = true;
+            // insert here the data
 
-            // Acceleration of speed
-            if(speed > maxSpeed[playerSelected]) speed = maxSpeed[playerSelected];
-            else {
-                if(speed < startSpeed[playerSelected]) speed = startSpeed[playerSelected];
-                speed += acceleration[playerSelected] * Time.deltaTime;
-            }
 
-            sisters[playerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = atualMovDirection * speed;
-        }
+            // ====================
 
-        if(Input.GetKeyDown(KeyCode.L)) atualMovDirection = Vector2.zero;
-
-    }
-
-    protected private void MovementCamera() {
-
-    }
-
-    void ChangeSister() {
-        if(playerSelected == 0) {
-            sisters[0].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            playerSelected = 1;
-        }
-        else {
-            sisters[1].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            playerSelected = 0;
+            reader.Close();
         }
     }
 
-    protected private class Data {
-        private float _exemplo;
+    [SerializeField] public class GameData {
+        private int _saveID;
+        private string _lastScene;
 
-        public float Exemplo {
-            get { return _exemplo; }
-            set { _exemplo = value; }
+        private Vector2[] _playerPosition;
+        private byte _lastPlayerSelected;
+
+        public int SaveID {
+            get { return _saveID; }
+            set { _saveID = value; }
         }
 
+        public string LastScene {
+            get { return _lastScene; }
+            set { _lastScene = value; }
+        }
+
+        public Vector2[] PlayerPosition {
+            get { return _playerPosition; }
+            set { _playerPosition = value; }
+        }
+
+        public byte LastPlayerSelected {
+            get { return _lastPlayerSelected; }
+            set { _lastPlayerSelected = value; }
+        }
+    }
+}
+
+[CustomEditor(typeof(GameManager))]
+public class ScriptEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        GameManager manager = (GameManager)target;
+
+        // Inspector code
 
     }
 }
---
+++ Assets/Systems/tempNemGameManager.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class tempNemGameManager : MonoBehaviour {
+
+
+    //public PlayerInputActions playerInputs;
+
+    // 0 = Yellow Sister / 1 = Purple Sister
+    [SerializeField] protected GameObject[] sisters = new GameObject[2]; // GameObject referent of the players
+    [SerializeField] protected float[] startSpeed = new float[2], maxSpeed = new float[2], acceleration = new float[2];
+    [SerializeField] protected Vector2 atualMovDirection; // direction of movment seted (prolonged)
+
+    protected private bool pause = true;
+    protected private int playerSelected = 0;
+    protected private bool inMovment = false;
+    protected private float speed = 0;
+
+    // Awake is called before the scene starts
+    protected private void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+        //playerInputs = GetComponent<PlayerInputActions>();
+
+        //playerInputs.PlayerActions.TouchTest.started += ctx => InputActions_onActionTriggered(ctx);
+        //playerInputs.PlayerActions.Movment.started += ctx => InputActions_onActionTriggered(ctx);
+    }
+
+    // Save game data and set start variables
+    protected private void SaveData(int saveID) {
+
+    }
+
+    // Load game data and get start variables
+    protected private void LoadData(int saveID) {
+
+    }
+
+    private void InputActions_onActionTriggered(InputAction.CallbackContext context) {
+        Debug.Log(context.phase);
+        //Debug.Log(playerInputs.PlayerActions.TouchTest.ReadValue<Vector2>());
+    }
+
+    // Start is called in the first frame of the scene
+    protected private void Start() {
+        StartPhase(); // temporario
+    }
+
+    // Sets the direction the player chooses
+    protected private Vector2 MovDirection() {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+            return Vector2.right;
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+            return Vector2.left;
+        else if(Input.GetAxisRaw("Vertical") > 0)
+            return Vector2.up;
+        else if(Input.GetAxisRaw("Vertical") < 0)
+            return Vector2.down;
+        else
+            return Vector2.zero;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if(pause != false) {
+            MovementSister();
+            MovementCamera();
+
+            if(inMovment == false && Input.GetKeyDown(KeyCode.P))
+                ChangeSister();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+            LoadScene("none",1);
+    }
+
+    protected private void LoadScene(string scene,int sceneID) {
+        if(scene == "none")
+            SceneManager.LoadSceneAsync(sceneID);
+        else
+            SceneManager.LoadScene(scene);
+
+        StartPhase();
+    }
+
+    // Load Start Atributts in the phase
+    protected private void StartPhase() {
+        // Find the sisters game objects in the scene
+        for(int i = 0; i < sisters.Length; i++)
+            sisters[i] = GameObject.Find("Player" + i.ToString());
+    }
+
+    protected private void MovementSister() {
+
+        // o jogador so podera trocar a direção quando o vector de direção prolongada for igual a zero
+        if(atualMovDirection == Vector2.zero) {
+            sisters[playerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            inMovment = false;
+            speed = 0;
+
+            if(MovDirection().x != 0)
+                atualMovDirection.x = MovDirection().x;
+            else if(MovDirection().y != 0)
+                atualMovDirection.y = MovDirection().y;
+        }
+        else {
+            inMovment = true;
+
+            // Acceleration of speed
+            if(speed > maxSpeed[playerSelected])
+                speed = maxSpeed[playerSelected];
+            else {
+                if(speed < startSpeed[playerSelected])
+                    speed = startSpeed[playerSelected];
+                speed += acceleration[playerSelected] * Time.deltaTime;
+            }
+
+            sisters[playerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = atualMovDirection * speed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.L))
+            atualMovDirection = Vector2.zero;
+
+    }
+
+    protected private void MovementCamera() {
+
+    }
+
+    void ChangeSister() {
+        if(playerSelected == 0) {
+            sisters[0].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            playerSelected = 1;
+        }
+        else {
+            sisters[1].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            playerSelected = 0;
+        }
+    }
+
+    protected private class Data {
+        private float _exemplo;
+
+        public float Exemplo {
+            get { return _exemplo; }
+            set { _exemplo = value; }
+        }
+
+
+    }
+}
