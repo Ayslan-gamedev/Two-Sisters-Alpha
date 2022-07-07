f50cf576a537bfa516db4abb67372d9ccd0822f4
Ayslan-gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-07-07 19:44:05 -0300

    alteração na movimentação

--- Assets/Systems/GameManager.cs
+++ Assets/Systems/GameManager.cs
@@ -9,8 +9,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-
-    private protected byte pause = 0;
+    [SerializeField] private protected byte pause = 0;
 
     // Player Controll Variables
     private const string PLAYER_OBJECT_KEY = "Player";
@@ -19,10 +18,12 @@ public class GameManager : MonoBehaviour {
     private protected GameObject[] sisters = new GameObject[2];
     private protected byte currentPlayerSelected = 0;
 
-    private protected float currentSpeed;
-    private protected readonly float[] acceleration, slowdown, startSpeed, maxSpeed;
-    private protected Vector2 currentMoveDirection = Vector2.zero;
-    private protected bool inMovement = false;
+    private protected float currentSpeed = 0;
+    [SerializeField] private protected float[] acceleration = new float[2], startSpeed = new float[2], maxSpeed = new float[2];
+    [SerializeField] private protected Vector2 currentMoveDirection = Vector2.zero;
+    [SerializeField] private protected bool inMovement = false;
+
+    [SerializeField] RaycastHit2D currentRay;
 
     // Save System Variables
     private protected const string FILE_LOCATE_SAVE_KEY = "/SaveData/save";
@@ -39,7 +40,16 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     private void Update() {
         if(pause != 1) {
-            MovementSister();
+            if(sisters[currentPlayerSelected] == null) {
+                sisters[currentPlayerSelected] = GameObject.Find(PLAYER_OBJECT_KEY + currentPlayerSelected.ToString());
+                switch(currentPlayerSelected) {
+                    case 0: currentPlayerSelected = 1; break;
+                    case 1: currentPlayerSelected = 0; break;
+                }
+            }
+            else 
+                MovementSister();
+
             if(inMovement == false && Input.GetKeyDown(KeyCode.P)) ChangeSister();
         }
 
@@ -57,28 +67,47 @@ public class GameManager : MonoBehaviour {
     }
 
     protected private void MovementSister() {
+        if(inMovement == true) {
+            sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = currentMoveDirection * currentSpeed;
+            
+            if(currentMoveDirection == Vector2.up)
+                currentRay = Physics2D.Raycast(sisters[currentPlayerSelected].transform.localPosition,Vector2.up,0.5f,1 << 3);
+            else if(currentMoveDirection == Vector2.down)
+                currentRay = Physics2D.Raycast(sisters[currentPlayerSelected].transform.localPosition,Vector2.down,0.5f,1 << 3);
+            else if(currentMoveDirection == Vector2.left)
+                currentRay = Physics2D.Raycast(sisters[currentPlayerSelected].transform.localPosition,Vector2.left,0.5f,1 << 3);
+            else
+                currentRay = Physics2D.Raycast(sisters[currentPlayerSelected].transform.localPosition,Vector2.right,0.5f,1 << 3);
+
+            if(currentRay.collider) {
+                currentMoveDirection = Vector2.zero;
+                inMovement = false;
+            }
+        }
 
         // the player can only change the direction when the extended direction vector is equal to zero
         if(currentMoveDirection == Vector2.zero) {
             sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            inMovement = false;
             currentSpeed = 0;
 
-            if(MoveDirecion().x != 0) currentMoveDirection.x = MoveDirecion().x;
-            else if(MoveDirecion().y != 0) currentMoveDirection.y = MoveDirecion().y;
+            if(MoveDirecion().x != 0)
+                currentMoveDirection.x = MoveDirecion().x;
+            else if(MoveDirecion().y != 0)
+                currentMoveDirection.y = MoveDirecion().y;
         }
         else {
             inMovement = true;
 
             // Acceleration of speed
-            if(currentSpeed > maxSpeed[currentPlayerSelected]) currentSpeed = maxSpeed[currentPlayerSelected];
+            if(currentSpeed > maxSpeed[currentPlayerSelected])
+                currentSpeed = maxSpeed[currentPlayerSelected];
             else {
-                if(currentSpeed < startSpeed[currentPlayerSelected]) currentSpeed = startSpeed[currentPlayerSelected];
+                if(currentSpeed < startSpeed[currentPlayerSelected])
+                    currentSpeed = startSpeed[currentPlayerSelected];
                 currentSpeed += acceleration[currentPlayerSelected] * Time.deltaTime;
             }
-
-            sisters[currentPlayerSelected].gameObject.GetComponent<Rigidbody2D>().velocity = currentMoveDirection * currentSpeed;
         }
+
     }
 
     private protected void ChangeSister() {
@@ -90,10 +119,10 @@ public class GameManager : MonoBehaviour {
     }
 
     protected private void LoadScene(string scene,int sceneID) {
-        if(scene == String.Empty)
-            SceneManager.LoadSceneAsync(sceneID);
-        else
-            SceneManager.LoadScene(scene);
+        if(scene == String.Empty) SceneManager.LoadSceneAsync(sceneID);
+        else SceneManager.LoadScene(scene);
+
+        if(sceneID > 0 && scene != "MainMenu") pause = 0;
     }
 
     // Save Data
@@ -131,7 +160,8 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    [SerializeField] public class GameData {
+    [SerializeField]
+    public class GameData {
         private int _saveID;
         private string _lastScene;
 
@@ -157,16 +187,5 @@ public class GameManager : MonoBehaviour {
             get { return _lastPlayerSelected; }
             set { _lastPlayerSelected = value; }
         }
-    }
-}
-
-[CustomEditor(typeof(GameManager))] 
-public class ScriptEditor : Editor {
-    public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
-        GameManager manager = (GameManager)target;
-
-        // Inspector code
-
     }
 }
